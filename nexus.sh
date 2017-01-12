306cb1321ac6792b129dd8aba68f0fd54b8c4766
Johannes Brännström <jbrannst@redhat.com>
2017-01-12 21:35:25 +0100

    aws tech forum settings

--- nexus.sh
+++ nexus.sh
@@ -2,4 +2,5 @@
 : “${APP_DOMAIN:?Need to set APP_DOMAIN non-empty. Ex rhel-cdk.10.1.2.2.xip.io or apps.openshift.com }”
 oc new-project ci
 oc new-app -f https://raw.githubusercontent.com/OpenShiftDemos/nexus/master/nexus2-persistent-template.yaml
-export MAVEN_MIRROR_URL=http://nexus-ci.$APP_DOMAIN/nexus/content/groups/public/
+export MAVEN_MIRROR_URL=http://nexus-ci.$APP_DOMAIN/content/groups/public/
+echo MAVEN_MIRROR_URL=http://nexus-ci.$APP_DOMAIN/content/groups/public/
--- ocp-infra.sh
+++ ocp-infra.sh
@@ -1,4 +1,5 @@
-export APP_DOMAIN=rhel-cdk.10.1.2.2.xip.io
+#export APP_DOMAIN=rhel-cdk.10.1.2.2.xip.io
+export APP_DOMAIN=apps.ocp.rocks
 ./metrics.sh
 ./nexus.sh
-./gogs.sh
+#./gogs.sh
